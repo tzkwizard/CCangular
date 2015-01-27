7f8aad8fcc907eedef6fa5f1f03779065b58c9d2
tzkwizard <tzkwizards@gmail.com>
2015-01-27 09:31:51 -0600

    1th commit

---
+++ WebApplication2/app/services/datacontext.js
@@ -0,0 +1,65 @@
+(function () {
+    'use strict';
+
+    var serviceId = 'datacontext';
+    angular.module('app').factory(serviceId, ['common','entityManagerFactory', datacontext]);
+
+    function datacontext(common, emFactory) {
+        var EntityQuery = breeze.EntityQuery;
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(serviceId);
+        
+        var logError = getLogFn(serviceId, 'error');
+        var logSuccess = getLogFn(serviceId, 'success');
+        var manager = emFactory.newManager();
+        var $q = common.$q;
+
+        var service = {
+            getPeople: getPeople,
+            getMessageCount: getMessageCount,
+            getSessionPartials: getSessionPartials
+        };
+
+        return service;
+
+        function getMessageCount() { return $q.when(72); }
+
+        function getPeople() {
+            var people = [
+                { firstName: 'John', lastName: 'Papa', age: 25, location: 'Florida' },
+                { firstName: 'Ward', lastName: 'Bell', age: 31, location: 'California' },
+                { firstName: 'Colleen', lastName: 'Jones', age: 21, location: 'New York' },
+                { firstName: 'Madelyn', lastName: 'Green', age: 18, location: 'North Dakota' },
+                { firstName: 'Ella', lastName: 'Jobs', age: 18, location: 'South Dakota' },
+                { firstName: 'Landon', lastName: 'Gates', age: 11, location: 'South Carolina' },
+                { firstName: 'Haley', lastName: 'Guthrie', age: 35, location: 'Wyoming' }
+            ];
+            return $q.when(people);
+        }
+
+
+        function getSessionPartials() {
+            var orderBy = 'timeSlotId,level,speaker.firstName';
+            var sessions;
+
+            return EntityQuery.from('Sessions')
+            .select('id,title,code,speakerId,trackId,timeSlotId,roomId,level,tags')
+            .orderBy(orderBy)
+            .toType('Session')
+            .using(manager).execute()
+            .to$q(querySucceeded, _queryFailed);
+
+            function querySucceeded(data) {
+                sessions = data.results;
+                log('Retrieved [Session Partials] from remote data source', sessions.length, true);
+                return sessions;
+            }
+            function _queryFailed(error) {
+                var msg = config.appErrorPrefix + 'Error retreiving data.' + error.message;
+                logError(msg, error);
+                throw error;
+            }   
+
+        }
+    }
+})();
---
+++ WebApplication2/app/session/sessions.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'sessions';
+    angular
+        .module('app')
+        .controller(controllerId, ['common', 'datacontext', sessions]);
+
+    // sessions.$inject = ['$location']; 
+
+    function sessions(common, datacontext) {
+        /* jshint validthis:true */
+        var vm = this;
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        
+        vm.title = 'Sessions';
+        vm.sessions = [];
+        vm.activate = activate;
+        activate();
+
+        function activate() {
+            var promises = [getSessions()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Sessions View'); });
+        }
+        function getSessions(){
+            return datacontext.getSessionPartials().then(function (data) {
+              return vm.sessions = data;
+        });
+        }
+    }
+})();
